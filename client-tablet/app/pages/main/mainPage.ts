272faab04df867596501a5707f64cb873008e8f3
privideph <pejotr+github@gmail.com>
2016-04-23 12:46:31 +0200

    main page

--- client-tablet/app/pages/main/mainPage.ts
+++ client-tablet/app/pages/main/mainPage.ts
@@ -1,10 +1,36 @@
-import {Page} from 'ionic-angular';
+import {Page, Alert, NavController} from 'ionic-angular';
+import {Navigation} from '../../providers/navigation/navigation';
 
 @Page({
-    templateUrl: 'build/pages/main/main-page.html'
+    templateUrl: 'build/pages/main/main-page.html',
+    providers: [Navigation]
 })
 export class MainPage {
-    constructor() {
 
+    constructor(protected nav: Navigation, protected nc: NavController) {
+
+    }
+
+    onCodeMissing() {
+        let alert = Alert.create({
+            title: 'Brak kodu? Idź do obsługi',
+            buttons: [
+                {
+                    text: 'Chwila, mam kod',
+                    handler: () => {}
+                },
+                {
+                    text: 'Ok, idę',
+                    handler: () => {
+                        this.nav.openMainPage();
+                    }
+                }
+            ]
+        });
+        this.nc.present(alert);
+    }
+
+    onPageDidEnter() {
+        // reset user data
     }
 }
