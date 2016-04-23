5a5d02da3b456fab21f7ac73ad102ef6cd187a4f
wgrabowski <wojtek@wojciech-grabowski.pl>
2016-04-23 13:10:19 +0200

    Added routing

--- client-desktop/src/app/components/events-app/events-app.ts
+++ client-desktop/src/app/components/events-app/events-app.ts
@@ -1,18 +1,19 @@
 import {Component} from 'angular2/core';
 import {MembersManagement} from  './../members-management/members-management';
+import {PackAdmin} from  './../pack-admin/pack-admin';
 import {RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
 
 @Component({
-    selector: 'events-app',
-    directives: [ROUTER_DIRECTIVES, MembersManagement],
-    templateUrl: 'app/components/events-app/events-app.html',
-    styleUrls: ['app/components/events-app/events-app.scss']
+  selector: 'events-app',
+  directives: [ROUTER_DIRECTIVES, MembersManagement],
+  templateUrl: 'app/components/events-app/events-app.html',
+  styleUrls: ['app/components/events-app/events-app.scss']
 })
 @RouteConfig([
-    {name: 'PackAdmin', path: '/pack-admin'}
+  {name: 'ConfirmPackage', path: '/pack-admin', component: PackAdmin,useAsDefault:true}
 ])
 export class EventsApp {
 
-    constructor() {
-    }
+  constructor() {
+  }
 }
