8bb88841a6a334c03a8d6bb5e62adc277bb94062
Mateusz Gawliński <matewka@gmail.com>
2016-04-23 13:03:54 +0200

    Added submit event emitter to num-pad.

--- client-tablet/app/components/num-pad/numPad.ts
+++ client-tablet/app/components/num-pad/numPad.ts
@@ -1,18 +1,29 @@
-import {Component} from 'angular2/core';
+import {Component, EventEmitter} from 'angular2/core';
 import {NumPadDisplay} from '../num-pad-display/numPadDisplay';
 import {NumPadKeyboard} from '../num-pad-keyboard/numPadKeyboard';
 
 @Component({
+  outputs: [ 'submit' ],
   selector: 'num-pad',
   templateUrl: 'build/components/num-pad/num-pad.html',
   directives: [NumPadDisplay, NumPadKeyboard]
 })
 export class NumPad {
+  submit = new EventEmitter<String>();
   value:string = '';
+  maxLength:number = 4;
 
   onPressed(value:string) {
-    if (this.value.length < 4) {
+    if (this.value.length < this.maxLength) {
       this.value += value.toString();
+    }
+
+    this.submit.emit(this.value);
+  }
+
+  onSubmit() {
+    if (this.value.length === this.maxLength) {
+      this.submit.emit(this.value);
     }
   }
 }
