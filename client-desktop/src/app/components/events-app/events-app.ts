06559403437612ba95051675defe821274f96760
patroner <patroner1@gmail.com>
2016-04-23 11:47:19 +0200

    Members management component, Search component

--- client-desktop/src/app/components/events-app/events-app.ts
+++ client-desktop/src/app/components/events-app/events-app.ts
@@ -1,14 +1,12 @@
 import {Component} from 'angular2/core';
 import {MembersManagement} from  './../members-management/members-management';
-import {Router, RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
-
+import {RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
 
 @Component({
     selector: 'events-app',
-    providers: [],
-    pipes: [],
     directives: [ROUTER_DIRECTIVES, MembersManagement],
     templateUrl: 'app/components/events-app/events-app.html',
+    styleUrls: ['app/components/events-app/events-app.scss']
 })
 @RouteConfig([
     {name: 'PackAdmin', path: '/pack-admin'}
--- client-desktop/src/app/components/members-management/members-management.ts
+++ client-desktop/src/app/components/members-management/members-management.ts
@@ -1,49 +1,63 @@
 import {Component, OnInit} from 'angular2/core';
+import {AppSettings} from '../../common/app-settings'
+import {SearchMember} from '../../components/search-member/search-member'
 
 @Component({
-    selector: 'members-management',
-    providers: [],
-    pipes: [],
-    templateUrl: 'app/components/members-management/members-management.html',
-    styleUrls: ['app/components/members-management/members-management.scss']
+  selector: 'members-management',
+  directives: [SearchMember],
+  templateUrl: 'app/components/members-management/members-management.html',
+  styleUrls: ['app/components/members-management/members-management.scss']
 })
 
-export class MembersManagement implements OnInit{
-    members: any;
+export class MembersManagement implements OnInit {
+  members:any;
 
-    constructor() {
-    }
+  constructor() {
+  }
 
-    ngOnInit(){
-        this.getList();
-    }
+  ngOnInit() {
+    this.getList();
+  }
 
-    getList(){
-        this.members = [
-            {
-                "name": "Ivan Disapierd",
-                "email": "mosssampson@gracker.com",
-                "phone": "+1 (834) 524-2682",
-                "consent": false,
-                "status": "not accepted",
-                "registered": "2014-10-28T12:39:38 -01:00",
-                "technologies": [
-                    "Javascript",
-                    "PHP"
-                ]
-            },
-            {
-                "name": "Crisitna Pitolina",
-                "email": "deliawooten@gracker.com",
-                "phone": "+1 (923) 578-2242",
-                "consent": true,
-                "status": "not accepted",
-                "registered": "2015-10-13T04:18:54 -02:00",
-                "technologies": [
-                    "Javascript",
-                    "PHP"
-                ]
-            }
+  isAccepted(status) {
+    return status === AppSettings.MEMBER_STATUSES.ACCEPTED;
+  }
+
+  acceptMember(member) {
+    const statuses = {
+      [AppSettings.MEMBER_STATUSES.ACCEPTED]: AppSettings.MEMBER_STATUSES.NOT_ACCEPTED,
+      [AppSettings.MEMBER_STATUSES.NOT_ACCEPTED]: AppSettings.MEMBER_STATUSES.ACCEPTED
+    };
+
+    member.status = statuses[member.status];
+  }
+
+  getList() {
+    this.members = [
+      {
+        "name": "Ivan Disapierd",
+        "email": "mosssampson@gracker.com",
+        "phone": "+1 (834) 524-2682",
+        "consent": false,
+        "status": "accepted",
+        "registered": "2014-10-28T12:39:38 -01:00",
+        "technologies": [
+          "Javascript",
+          "PHP"
         ]
-    }
+      },
+      {
+        "name": "Crisitna Pitolina",
+        "email": "deliawooten@gracker.com",
+        "phone": "+1 (923) 578-2242",
+        "consent": true,
+        "status": "not accepted",
+        "registered": "2015-10-13T04:18:54 -02:00",
+        "technologies": [
+          "Javascript",
+          "PHP"
+        ]
+      }
+    ]
+  }
 }
